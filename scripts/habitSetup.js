82f445212cd2521badff64ad7bb3961de4265e4c
AlexanderVanVugt <33684938+AlexanderVanVugt@users.noreply.github.com>
2017-11-23 21:37:10 +0100

    Update habitSetup.js

--- scripts/habitSetup.js
+++ scripts/habitSetup.js
@@ -1,5 +1,11 @@
-document.getElementById("createHabitBttn").onclick = function(){openHabitSetup()};
-function openHabitSetup(){
-  document.getElementById("habitSetup").innerHTML = setupToString;
+function(){openCloseHabitSetup("close")};
+function openCloseHabitSetup(x){
+  if(x == "open"){
+    document.getElementById("habitSetup").innerHTML = setupToString;
+    document.getElementById("saveHabitChanges").onclick = openCloseHabitSetup("close")
+  }else if(x == "close"){
+    document.getElementById("habitSetup").innerHTML = '<button id="createHabitBttn">Create habit</button>';
+    document.getElementById("createHabitBttn").onclick = function(){openHabitSetup("open")};
 }
-var setupToString = "<p>The habit setup will be implemented soon.</p>";
+var setupToString = "<p>The habit setup will be implemented soon.</p>"+
+    "<button id='saveHabitChanges'>Commit</button>";
