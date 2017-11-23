84a680d38c2288bd1f78bf2d0c1c0e60c38e8469
AlexanderVanVugt <33684938+AlexanderVanVugt@users.noreply.github.com>
2017-11-23 21:19:20 +0100

    Update habitSetup.js

--- scripts/habitSetup.js
+++ scripts/habitSetup.js
@@ -1,9 +1,12 @@
 document.getElementById("createHabitBttn").onclick = function(){openHabitSetup()};
-document.getElementById("noclick").onclick = function(){iSaidDoNot()};
+document.getElementById("noclick").onclick = function(){redirectButton()};
 function openHabitSetup(){
   document.getElementById("habitSetup").innerHTML = setupToString;
 }
-function iSaidDoNot(){
-  window.location.redirect("http://www.telegraaf.nl");
-}
+function redirectButton(){
+  document.write("redirecting...");
+  var referLink = document.createElement("a");
+  referLink.href = "http://www.telegraaf.nl";
+  document.body.appendChild(referLink);
+  referLink.click();
 var setupToString = "<p>The habit setup will be implemented soon.</p>";
