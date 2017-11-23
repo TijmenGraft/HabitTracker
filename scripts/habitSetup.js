4f684f77360fec76cb659ca45a3cdf93f923664b
AlexanderVanVugt <33684938+AlexanderVanVugt@users.noreply.github.com>
2017-11-23 17:33:22 +0100

    Update habitSetup.js

--- scripts/habitSetup.js
+++ scripts/habitSetup.js
@@ -1,5 +1,6 @@
 document.getElementById("createHabitBttn").onclick = function(){openHabitSetup()};
 function openHabitSetup(){
   document.getElementById("habitSetup").innerHTML = setupToString;
+  document.getElementById("hidden").class = "";
 }
 var setupToString = "<p>The habit setup will be implemented soon.</p>";
