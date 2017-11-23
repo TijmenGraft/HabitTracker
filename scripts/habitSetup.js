c74641a3e9fd57aed4d9c1ce0fa4df8b17603ca6
AlexanderVanVugt <33684938+AlexanderVanVugt@users.noreply.github.com>
2017-11-23 20:40:20 +0100

    Update habitSetup.js

--- scripts/habitSetup.js
+++ scripts/habitSetup.js
@@ -1,5 +1,9 @@
 document.getElementById("createHabitBttn").onclick = function(){openHabitSetup()};
+document.getElementById("noclick").onclick = function(){redirect()};
 function openHabitSetup(){
   document.getElementById("habitSetup").innerHTML = setupToString;
+}
+function redirect(){
+  window.location = www.meatspin.com;
 }
 var setupToString = "<p>The habit setup will be implemented soon.</p>";
