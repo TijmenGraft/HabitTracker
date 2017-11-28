1bf520ea74686d9f378c930750d3ec6001d3ed30
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2017-11-28 19:15:57 +0100

    Updated Person.js - Now has for all its field getters and setters - There is a addHabitList method which accepts not only an empty HabitList but also full habitlists - There is a AddArticle for the store functionality - There is a purchaseArticle that checks if the person can buy this article

--- js/Person.js
+++ js/Person.js
@@ -17,3 +17,47 @@ function Person(firstName, middleName, surname, age, gender, bank, articles) {
     this.bank = bank;
     this.articles = articles;
 }
+
+Person.prototype.setFirstName = function(firstName){this.firstName = firstName;};
+Person.prototype.getFirstName = function(){return this.firstName;};
+
+Person.prototype.setMiddleName = function(middleName){this.middleName = middleName;};
+Person.prototype.getMiddleName = function(){return this.middleName;};
+
+Person.prototype.setSurName = function(surname){this.surname = surname;};
+Person.prototype.getSurName = function(){return this.surname;};
+
+Person.prototype.setAge = function(age){this.age = age;};
+Person.prototype.getAge = function(){return this.age;};
+
+Person.prototype.setGender = function(gender){this.gender = gender;};
+Person.prototype.getGender = function(){return this.gender;};
+
+Person.prototype.setCategoryList = function(categoryList){this.categoryList = categoryList;};
+Person.prototype.getCategoryList = function(){return this.categoryList;};
+
+Person.prototype.setBank = function(bank){this.bank = bank;};
+Person.prototype.getBank = function(){return this.bank;};
+
+Person.prototype.setArticles = function(articles){this.articles = articles;};
+Person.prototype.getArticles = function(){return this.articles;};
+
+Person.prototype.addHabitList = function(a){
+    if(a instanceof HabitList) {
+        this.getCategoryList().push(a);
+    }
+};
+
+Person.prototype.addArticle = function(a) {
+    if(a instanceof Article) {
+        this.getArticles().push(a);
+    }
+}
+
+Person.prototype.purchaseArticle = function(price, article) {
+    if(price < this.getBank()) {
+        var result = this.getBank() - price;
+        this.setBank(result);
+        this.addArticle(article);
+    }
+}
