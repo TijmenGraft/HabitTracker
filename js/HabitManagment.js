7e8579de2df7ea82f3bd64af030568cc1dc87aed
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2017-11-29 20:49:49 +0100

    Added more habit manangement

--- js/HabitManagment.js
+++ js/HabitManagment.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
     //General habit handeling 
     //this opens the model where the habit form will be contained
-    $(".add-habit").on("click", function(){
+    $(document).on("click", ".add-habit", function(){
         console.log("You have clicked on adding a habit");
+        var location = $(this).attr('id').replace("add_habit_to_","").toLowerCase();
+        $("#add_habit_model").attr({
+            "data-destination":location
+        });
     });
 
     $("#add_habit_submit").on("click", function(){
+        console.log(this);
         //id, name, type, category, frequency, description, startDate, endDate
         var habitTitle = $("#habit_main_title").val();
         var habitType = $('input[name=habit_form_type]:checked').val();
