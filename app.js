052e07ec672fe9c6dc47a75c5c233bc73a429299
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2017-12-05 10:59:51 +0100

    Added all those files which make this app interactive

---
+++ app.js
@@ -0,0 +1,118 @@
+"use strict";
+//Package required for having a normal working server
+const http = require("http");
+const fs = require("fs");
+const events = require("events");
+const nodemailer = require("nodemailer");
+const querystring = require("querystring");
+const formidable = require("formidable");
+const util = require("util");
+
+var eventEmitter = new events.EventEmitter();
+
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "tijmengraft@gmail.com",
+        pass: "Kungfu1998"
+    }
+});
+
+var mailOptions = {
+    from: "tijmengraft@gmail.com",
+    to: "tijmengraft@gmail.com",
+    subject: "this is a node.js test mail",
+    text: "<h1>easy</h1> message",
+    html: "<h1>Easy</h1>"
+}
+
+var server = http.createServer(function (req, res) {
+    if (req.method.toLowerCase() == 'get') {
+        displayPage(req,res);
+    } else if (req.method.toLowerCase() == 'post') {
+        //processAllFieldsOfTheForm(req, res);
+        processFormFieldsIndividual(req,res);
+    }
+});
+/**
+ * this fucntion will give us the displayPage when we call it
+ * @param {*} res 
+ */
+function displayPage(req,res) {
+    if(req.url.indexOf(".html") != -1) {
+        fs.readFile(__dirname + "splash.html", function(err, data) {
+            res.writeHead(200, {
+                "Content-type":"text/html",
+                "Content-length":data.length
+            });
+            res.write(data);
+            res.end();   
+        })
+    }
+    
+    if(req.url.indexOf(".css") != -1) {
+        fs.readFile(__dirname + "/style/framework.css", function(err,data){
+            if(err) {
+                console.log(err);
+            }
+            res.writeHead(200, {
+                "Content-Type": "text/css",
+                "Content-length": data.length
+            });
+            res.write(data);
+            res.end();
+        });
+    }
+};
+
+/**
+ * This method will handle forms when a post requist is coming in
+ * @param {*} req 
+ * @param {*} res 
+ */
+function processFormFieldsIndividual(req, res) {
+    var fields = [];
+    var form = new formidable.IncomingForm();
+    form.on("field", function(field,value){
+        console.log(field + " : " + value);
+        fields[field] = value;
+    });
+
+    form.on("file", function(name,file){
+        console.log(name + " : " + file);
+        fields[name] = file;
+    });
+
+    form.on("progress", function(bytesReceived, bytesExpected) {
+        var progress = {
+            type: "progress",
+            bytesReceived : bytesReceived,
+            bytesExpected : bytesExpected
+        };
+        console.log(progress);
+    });
+
+    form.on("end", function() {
+        res.writeHead(200, {
+            "content-type":"text/plain"
+        });
+        res.write("received the data: \n\n");
+        res.end(util.inspect({
+            fields: fields
+        }));
+    });
+    form.parse(req);
+}
+
+// transporter.sendMail(mailOptions, function(error, info) {
+//     if(error) {
+//         console.log(error);
+//     } 
+//     else {
+//         console.log("Email sent:"+ info.response);
+//     }
+// });
+
+server.listen(8080);
+
