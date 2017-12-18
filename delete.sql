efd7461090c3186ea6e754392b47ce376a6f0a5e
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2017-12-18 20:06:26 +0100

    update css

--- delete.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE `habitsdatabase`.`dates`;
-DROP TABLE `habitsdatabase`.`frequency`;
-DROP TABLE `habitsdatabase`.`habit`;
-DROP TABLE `habitsdatabase`.`habit_done`;
-DROP TABLE `habitsdatabase`.`habitlistcatelog`;
-DROP TABLE `habitsdatabase`.`user`;
