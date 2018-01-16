86fb560962a471af660e0f20403de7e160adea3f
Unknown <alexandervvugt@gmail.com>
2018-01-16 02:47:10 +0100

    error: require is not defined
    ?

--- client/js/shopmodules/shopscript.js
+++ client/js/shopmodules/shopscript.js
@@ -1,2 +1,13 @@
-var walletObject = require(./userwallet);
-var shop = require(./shop);
+var walletObject = require("userwallet");
+var shop = require("shop");
+
+document.onreadystatechange = function() {
+  if (document.readyState === 'complete') {
+  	document.getElementById("premiumCurrencyDisplay").innerHTML = walletObject["premiumCurrency"];
+  	document.getElementById("currencyDisplay").innerHTML = walletObject["currency"];
+
+    document.getElementById("premiumCurrencyIncrease").onclick = function(){
+		walletObject["premiumCurrency"] += 100;
+	};
+  };
+};
